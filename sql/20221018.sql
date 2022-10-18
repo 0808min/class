9963fb0f0509122a9b3c03470b4f5f077ca15b54
juyoni <juyoni2222@gmail.com>
2022-10-18 15:00:43 +0900

    Update 20221018.sql

--- sql/20221018.sql
+++ sql/20221018.sql
@@ -1,3 +1,31 @@
 -- 20221018
 
--- rownum
+-- inline view - rownum
+-- from 절 뒤에 오는 서브쿼리 -> inline view
+-- select -> 결과도 테이블
+
+select * from emp;
+
+select rownum, emp.* from emp order by hiredate;
+
+-- rownum : 사용자가 생성하는 컬럼이 아니다, 시스템에서 제공하는 컬럼
+
+select rownum, t.*
+from (select * from emp order by hiredate) t
+;
+-- top3
+-- 입사일이 가장 오래된 3명을 구하자
+
+select rownum, t.*
+from (select * from emp order by hiredate) t
+where rownum <= 3
+;
+
+-- my sql select * from emp order by hiredate (rownum = limit) 0, 3;
+
+
+
+
+
+
+
