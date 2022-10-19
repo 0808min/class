4683a6d85356707f58df1fe36dd0e57db2d38303
0808min <min0808mini@gmail.com>
2022-10-19 16:23:53 +0900

    s

--- JDBCProject/src/jdbc/UpdateTest.java
+++ JDBCProject/src/jdbc/UpdateTest.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class InsertTest {
+public class UpdateTest {
 
 	public static void main(String[] args) {
 
