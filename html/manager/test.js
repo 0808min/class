0b4fa90a2fd3fc9afd9cad69ad20422c7131b393
0808min <min0808mini@gmail.com>
2022-11-04 18:00:49 +0900

    Update test.js

--- html/manager/test.js
+++ html/manager/test.js
@@ -1,37 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const loginform = document.querySelector('#regForm')
+    const loginform = document.querySelector('#list')
     const userid = document.querySelector('#userID')
     const pw = document.querySelector('#pw')
     const repw = document.querySelector('#repw')
     const username = document.querySelector('#userName')
+const msg = document.querySelector('.msg')
 
 
-    document.addEventListener('submit', (event) => {
+    document.addEventListener('click', (event) => {
 
        
 
-        const userid = document.querySelector('#userID')
-        const pw = document.querySelector('#pw')
-        const repw = document.querySelector('#repw')
-
-        console.log(userid.value.search(/[a-zA-Z0-9]/))
-        console.log(/[a-zA-Z0-9]/.test(userid.value))
-
-        console.log(pw.value.search(/[a-zA-Z0-9]/))
-        console.log(/[a-zA-Z0-9]/.test(pw.value))
-
-        console.log(repw.value.search(/[a-zA-Z0-9]/))
-        console.log(/[a-zA-Z0-9]/.test(repw.value))
 
 
-        event.preventDefault()
-
-        
     })
 
-    document.addEventListener('click', (event) => {
-            
-    })
+    document.addEventListener(DOMC)
 
 })
 
