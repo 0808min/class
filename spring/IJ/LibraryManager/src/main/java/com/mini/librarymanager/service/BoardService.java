132c57070d6c7847f3758c55cc47cded3f4f712f
0808min <min0808mini@gmail.com>
2022-12-02 09:51:26 +0900

    mini

--- spring/IJ/LibraryManager/src/main/java/com/mini/librarymanager/service/BoardService.java
+++ spring/IJ/LibraryManager/src/main/java/com/mini/librarymanager/service/BoardService.java
@@ -1,33 +1,30 @@
-package com.mini.librarymanager.service;
+package com.mini.librarymanager.Service;
 
-import com.mini.librarymanager.domain.BoardDTO;
-import com.mini.librarymanager.domain.MemberDTO;
-import com.mini.librarymanager.mapper.BoardMapper;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.*;
+import java.util.*;
+import org.springframework.stereotype.*;
 
-import java.util.List;
+import com.mini.librarymanager.DAO.*;
+import com.mini.librarymanager.DTO.*;
 
 @Service
-@Log4j2
 public class BoardService {
+	private BoardDAO boardDAO;
+	private CommentDAO commentDAO;
 
-    @Autowired(required = false)
-    private BoardMapper boardMapper;
+	@Autowired
+	public BoardService(BoardDAO _boardDAO, CommentDAO _commentDAO) {
+		this.boardDAO = _boardDAO;
+		this.commentDAO = _commentDAO;
+	}
 
-
-    public List<BoardDTO> getBoardList() {
-        List<BoardDTO> list = null;
-        try {
-            list = boardMapper.showAll();
-            log.info(list);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return list;
-    }
-
+	public void updatePublic(BoardDTO _boardDTO, String inputBoardPublic) {
+		// 게시글 공개 여부 설정
+		boardDAO.updatePublic(_boardDTO, inputBoardPublic);
+	}
+	
+	// 게시글 마지막 댓글 아이디 가져오기
+	public int findLatestCID(int inputFBID) {
+		return commentDAO.findLatestCID(inputFBID);
+	}
 }
-
