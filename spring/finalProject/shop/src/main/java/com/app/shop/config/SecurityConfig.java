9e0dd416b5f7c55dd002cd0057641fe462dcbb1f
0808min <min0808mini@gmail.com>
2023-01-05 17:58:29 +0900

    project

--- spring/finalProject/shop/src/main/java/com/app/shop/config/SecurityConfig.java
+++ spring/finalProject/shop/src/main/java/com/app/shop/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig{
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
