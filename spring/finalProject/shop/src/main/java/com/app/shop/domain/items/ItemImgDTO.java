9b9c3a259b9d564b0e49e3fa33784d390c618c3e
0808min <min0808mini@gmail.com>
2023-01-09 17:51:38 +0900

    project

--- spring/finalProject/shop/src/main/java/com/app/shop/domain/items/ItemImgDTO.java
+++ spring/finalProject/shop/src/main/java/com/app/shop/domain/items/ItemImgDTO.java
@@ -1,5 +1,6 @@
 package com.app.shop.domain.items;
 
+import com.app.shop.entity.item.ItemImg;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -13,8 +14,23 @@ public class ItemImgDTO {
     private String imgUrl;
     private String repImgYn;
 
-    public ItemImgDTO(Long id, String imgName, String  oriImgName, String imgUrl, String repImgYn) {
-
+    public ItemImgDTO(String imgName, String  oriImgName, String imgUrl, String repImgYn) {
+        this.imgName = imgName;
+        this.oriImgName = oriImgName;
+        this.imgUrl = imgUrl;
+        this.repImgYn = repImgYn;
     }
+
+    public ItemImg toEntity(ItemImgDTO dto) {
+        ItemImg entity = ItemImg.builder()
+                .imgName(dto.imgName)
+                .oriImgName(dto.oriImgName)
+                .imgUrl(dto.imgUrl)
+                .repimgYn(dto.repImgYn)
+                .build();
+
+        return entity;
+    }
+    
 
 }
